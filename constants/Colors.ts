5bf0dad01cbef3b9310bbbcc6682d9ad430fef18
Adeayo <aadelee74@gmail.com>
2024-09-26 10:24:41 +0100

    Update colors and icons configuration

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,17 +3,18 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#FFA001';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#161622',
     tint: tintColorLight,
     icon: '#687076',
-    tabIconDefault: '#687076',
+    tabIconDefault: '#CDCDE0',
     tabIconSelected: tintColorLight,
+    borderTopColor: '#232533',
   },
   dark: {
     text: '#ECEDEE',
